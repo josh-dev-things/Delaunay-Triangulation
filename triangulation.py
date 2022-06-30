cdc3f8dc9b41e5b6e8fb17e088f3f2d2e3acccfc
EthanKessler <ethankessler.dev@gmail.com>
2022-06-30 15:43:18 +0100

    Fully works
    - Progress Bar [Best bit]
    - No overlapping points being generated (Unless RNG hates you lol)
    - Fixed other apparent bugs

--- triangulation.py
+++ triangulation.py
@@ -4,6 +4,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import utils
+import sys
+
+def progressbar(it, prefix="", size=60, out=sys.stdout): # Python3.3+
+    count = len(it)
+    def show(j):
+        x = int(size*j/count)
+        print("{}[{}{}] {}/{}".format(prefix, u"█"*x, "."*(size-x), j, count), 
+                end='\r', file=out, flush=True)
+    show(0)
+    for i, item in enumerate(it):
+        yield item
+        show(i+1)
+    print("\n", flush=True, file=out)
 
 class point:
     def __init__(self, x, y) -> None:
@@ -47,14 +60,30 @@ class triangle:
         self.generateCircumCircle()
 
     def generateCircumCircle(self):
-        p1 = self.p1
-        p2 = self.p2
-        p3 = self.p3
+        p1 = point(self.p1.X(), self.p1.Y())
+        p2 = point(self.p2.X(), self.p2.Y())
+        p3 = point(self.p3.X(), self.p3.Y())
 
-        m_1_2 = ((p1.X() - p2.X())/(p2.Y() - p1.Y())) #Finding the gradient of the bisector of points 1 & 2
-        m_2_3 = ((p2.X() - p3.X())/(p3.Y() - p2.Y())) # 2 & 3
+        #BUG
+        # if (p2.Y() - p1.Y()) != 0:
+        #     m_1_2 = ((p1.X() - p2.X())/(p2.Y() - p1.Y())) #Finding the gradient of the bisector of points 1 & 2
+        # else:
+        m_1_2 = ((p1.X() - p2.X())/(p2.Y() - p1.Y())) if (p2.Y() - p1.Y()) != 0 else ((p1.X() - p2.X())/(p2.Y() - p1.Y() + 0.000001))
+
+        # try:
+        #     m_2_3 = ((p2.X() - p3.X())/(p3.Y() - p2.Y())) # 2 & 3
+        # except:
+        #     self.generateCircumCircle(p3YOffset = p3.Y()/1000 + 1/1000)
+        #     return
+        m_2_3 = ((p2.X() - p3.X())/(p3.Y() - p2.Y())) if (p3.Y() - p2.Y()) != 0 else ((p2.X() - p3.X())/(p3.Y() - p2.Y() + 0.000001))
+        #BUG
+
         c_1_2 = ((p1.Y() + p2.Y())/2)-(((p1.X()+p2.X())/2) * m_1_2) #Finding c (y intercept, consult GCSE Maths)
         c_2_3 = ((p2.Y() + p3.Y())/2)-(((p2.X()+p3.X())/2) * m_2_3)
+        
+        if (m_2_3 - m_1_2) == 0: #This prevents a divide by zero error later on.
+            m_2_3 = m_2_3 + 0.000001
+
         x = (c_1_2 - c_2_3)/(m_2_3 - m_1_2)
         y = (m_1_2 * x) + c_1_2
         radius = np.sqrt(pow(p1.X()-x, 2) + pow(p1.Y()-y, 2))
@@ -62,7 +91,13 @@ class triangle:
         centrePoint = point(x, y)
         self.circumCircle = circumCircle(centrePoint, radius)
 
-    def draw(self):
+    def pointIsVertex(self, pointToCheck : point) -> bool:
+        if pointToCheck == self.p1 or pointToCheck == self.p2 or pointToCheck == self.p3:
+            return True
+        else:
+            return False
+
+    def draw(self, displayCircumCircle = False):
         p1 = self.p1
         p2 = self.p2
         p3 = self.p3
@@ -78,20 +113,25 @@ class triangle:
         x23, y23 = [p2x, p3x],[p2y, p3y]
         x13, y13 = [p1x, p3x],[p1y, p3y]
 
-        print(p1x)
+        #print(p1x)
         plt.plot(p1x, p1y, p2x, p2y, p3x, p3y, marker="o", c="black")
         plt.plot(x12,y12,x23,y23,x13,y13, marker='o', c="black")
-        circumCircle = patches.Circle(xy=(self.circumCircle.getCentre()), radius=self.circumCircle.radius, fill=False)
-        plt.gca().add_patch(circumCircle)
-        plt.axis('scaled')
+
+        if displayCircumCircle:
+            circumCircle = patches.Circle(xy=(self.circumCircle.getCentre()), radius=self.circumCircle.radius, fill=False, edgecolor="grey", alpha=0.1)
+            plt.gca().add_patch(circumCircle)
 
 class mesh:
     def __init__(self, initialTriangles : list) -> None:
         self.triangles = []
         for i in range(len(initialTriangles)):
             self.triangles.append(initialTriangles[i])
+    
+    def __str__(self) -> str:
+        return f"Mesh with {len(self.triangles)} triangles"
 
     def addPoint(self, newPoint : point):
+        #print(self)
         containingTriangles = []
         indicesToPop = []
         for i in range(len(self.triangles)):
@@ -101,7 +141,8 @@ class mesh:
                 # self.triangles.pop(i)
                 indicesToPop.append(i)
 
-        for index in indicesToPop:
+        for index in sorted(indicesToPop, reverse=True): #Iterate through it backwards to not throw off other index
+            #print(f"Popping triangle at index {index}. Len Triangles: {len(self.triangles)}")
             self.triangles.pop(index)
 
         remesh = mesh(containingTriangles)
@@ -123,7 +164,7 @@ class mesh:
             delta.append(point(dX, dY))
             repointATan2.append(np.arctan2(dX, dY))
 
-        print(repoint)
+        #print(repoint)
 
         #Sort (This may or may not work... who knows ~ Josh)
         for i in range(1, len(repoint)):
@@ -141,7 +182,7 @@ class mesh:
             repointATan2[j+1] = key
             repoint[j+1] = keyPoint
 
-        print(repoint)
+        #print(repoint)
 
         #Now make some triangles?
         for i in range(len(repoint)):
@@ -151,6 +192,9 @@ class mesh:
             newTriangle = triangle(p1, p2, newPoint)
             newTriangle.draw()
             self.triangles.append(newTriangle) #Add new triangle to mesh data
+        #print(self)
+
+        #self.draw()
 
 
 
@@ -164,31 +208,43 @@ class mesh:
         return isOverlap
 
     def draw(self):
-        plt.show()
+        plt.cla()
         for triangle__ in self.triangles:
-            triangle__.draw()
+            triangle__.draw(True)
+        #print("Drawn all triangles")
+        plt.axis('scaled')
+        plt.show()
+
+    def cullConnections(self, pointToCull : point):
+        for i in reversed(range(len(self.triangles))):
+            thisTriangle = self.triangles[i]
+            if thisTriangle.pointIsVertex(pointToCull):
+                self.triangles.pop(i)
 
 
 
 
 
 if __name__ == "__main__":
-    numberOfPoints = int(input("Enter the number of points to generate: "))
-    # p1 = point(random.random(), random.random())
-    # p2 = point(random.random(), random.random())
-    # p3 = point(random.random(), random.random())
-    a1 = point(0.5, 10) #Anchor that will be eventually deleted. Anchors form the initial triangle
-    a2 = point(-10, -10)
-    a3 = point(9, -9)
+    a1 = point(50, 100) #Anchor that will be eventually deleted. Anchors form the initial triangle
+    a2 = point(-100, -100)
+    a3 = point(90, -90)
     
     points = [] #Point cloud
     masterTriangle = triangle(a1, a2, a3)
     trianglesForMesh = [masterTriangle]
     thisMesh = mesh(trianglesForMesh)
-    for i in range(numberOfPoints):
-        newPoint = point(random.random(), random.random())
+    n = int(input("Enter number of points to generate: "))
+    for i in progressbar(range(n), prefix="Triangulating Mesh: "):
+        plt.cla()
+        newPoint = point(random.random() * 10, random.random() * 10) #BUG Does not like overlapping points
         thisMesh.addPoint(newPoint)
         # points.append(newPoint)
         plt.plot(newPoint.X(), newPoint.Y(), marker="o", c="teal")
+        # plt.show()
 
-    plt.show()
+    anchors = [a1, a2, a3]
+    for i in progressbar(range(3), prefix="Culling Anchors: "):
+        thisMesh.cullConnections(anchors[i])
+
+    thisMesh.draw()
